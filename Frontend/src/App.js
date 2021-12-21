5543a847ea65c3f08485d692e83257923ed6bbd3
Debarshi Mondal <debopiku1122@gmail.com>
2021-12-21 18:50:57 +0530

    initial-setup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>
-        <span style={{ color: "salmon" }}>E-appointment</span> Hello From React
+        <span style={{ color: "salmon" }}>E-appointment</span> Hello From React 17.10
       </h1>
     </div>
   );
