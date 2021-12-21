95026909ba9e81249a92d9ce0da2c0b965de4d14
Debarshi Mondal <debopiku1122@gmail.com>
2021-12-21 18:49:41 +0530

    initial-setup

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        <span style={{ color: "salmon" }}>E-appointment</span> Hello From React
+      </h1>
+    </div>
+  );
+};
+
+export default App;
