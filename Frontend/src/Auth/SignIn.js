e59954260b75c3df3b7afdf239f665dd020104e9
Debarshi Mondal <debopiku1122@gmail.com>
2021-12-28 16:57:14 +0530

    bugs

--- Frontend/src/Auth/SignIn.js
+++ Frontend/src/Auth/SignIn.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
 
 const SignIn = () => {
+  const [value, setValue] = useState({ email: "", password: "" });
+  const { email, password } = value;
+
+  var poolData = {
+    UserPoolId: "ap-south-1_ODkATGGHL", 
+    ClientId: "2c1793ve4nmbq5jj1t2hjkf08s",
+  };
+
+  const authenicate = async () => {
+    return await new Promise((resolve, reject) => {
+      const user = new CognitoUser({
+        Username: email,
+        Pool: poolData,
+      });
+      const authDetails = new AuthenticationDetails({
+        Password: password,
+        Username: email,
+      });
+      user.authenticateUser(authDetails, {
+        onSuccess: (res) => {
+          // setCookie("auth_Token", res.getIdToken());
+          resolve(res);
+        },
+        onFailure: (err) => {
+          reject(err);
+        },
+      });
+    });
+  };
+
   return (
     <div className="section--login">
       <h1>Welcome Back!</h1>
@@ -9,7 +40,12 @@ const SignIn = () => {
       <form className="login_card">
         <div className="col-8">
           <label className="form-label">Username or Email Address</label>
-          <input type="text" className="form-control" />
+          <input
+            value={email}
+            onChange={(e) => setValue({ ...value, email: e.target.value })}
+            type="email"
+            className="form-control"
+          />
         </div>
         <div className="col-8 mt-3">
           <label className="form-label">
@@ -18,10 +54,17 @@ const SignIn = () => {
               Forgot?
             </Link>
           </label>
-          <input type="text" className="form-control" />
+          <input
+            value={password}
+            onChange={(e) => setValue({ ...value, password: e.target.value })}
+            type="password"
+            className="form-control"
+          />
         </div>
-        <div className="col-8 signin--btn">
-          <button className="btn">Sign In</button>
+        <div className="col-8 signin--btn" onClick={() => authenicate()}>
+          <button type="button" className="btn">
+            Sign In
+          </button>
         </div>
       </form>
     </div>
