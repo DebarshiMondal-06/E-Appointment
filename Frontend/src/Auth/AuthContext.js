086eb1502fb18faf1b36934e2f3af677779bc382
Debarshi Mondal <debopiku1122@gmail.com>
2022-01-07 01:11:37 +0530

    updated_code_auth_validaiton-3

--- Frontend/src/Auth/AuthContext.js
+++ Frontend/src/Auth/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { createContext } from 'react';
+import 'cross-fetch/polyfill';
 import { AuthenticationDetails, CognitoUser, CognitoUserAttribute, CognitoUserPool } from 'amazon-cognito-identity-js';
 import Swal from 'sweetalert2';
 
