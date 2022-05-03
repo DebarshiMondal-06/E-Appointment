c02fdb6f62b242da46b420fa25a8bf2c10eafef0
Debarshi Mondal <debopiku1122@gmail.com>
2022-05-03 22:14:26 +0530

    updated dashboard ui

--- Frontend/src/Pages/DashboardCard.js
+++ Frontend/src/Pages/DashboardCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
 import { FaUser } from 'react-icons/fa';
 
 const DashboardCard = () => {
+  const [cookie] = useCookies();
+  const { user_role } = cookie.user_data || {};
 
   let dashboard_card = (textcolor, text, count) => {
     return <div className='dashboard--card card shadow'>
@@ -16,16 +19,34 @@ const DashboardCard = () => {
   return <>
     <h1 className=''>Dashboard</h1>
     <main className='dashboard--main'>
-      {dashboard_card('success', 'Total User', 44)}
-      {dashboard_card('danger', 'Pending User', 44)}
-      {dashboard_card('info', 'Total Patient', 17)}
-      {dashboard_card('warning', 'Daily Patient', 12)}
-      {dashboard_card('secondary', 'Total Doctor', 32)}
-      {dashboard_card('danger', 'Daily Doctor', 18)}
-      {dashboard_card('success', 'Total Hospitals', 29)}
-      {dashboard_card('info', 'Appointments', 53)}
-      {dashboard_card('warning', 'Payments', 25)}
-      {dashboard_card('secondary', 'Revenue', 3700)}
+      {(user_role === 'admin') ?
+        <> {dashboard_card('success', 'Total User', 44)}
+          {dashboard_card('danger', 'Pending User', 44)}
+          {dashboard_card('info', 'Total Patient', 17)}
+          {dashboard_card('secondary', 'Total Doctor', 32)}
+          {dashboard_card('success', 'Total Hospitals', 29)}
+          {dashboard_card('secondary', 'Revenue', 3700)}
+        </>
+        : null}
+
+      {(user_role === 'doctor') ? <>
+        {dashboard_card('success', 'Patient', 17)}
+        {dashboard_card('danger', 'Appointments', 5)}
+        {dashboard_card('info', 'Upcoming Schedule', 5)}
+        {dashboard_card('secondnary', 'Payments', 2)}
+        {dashboard_card('warning', 'Profit', 2)}
+      </>
+        : null}
+
+      {(user_role === 'patient') ? <>
+        {dashboard_card('warning', 'Appointments', 5)}
+        {dashboard_card('info', 'Upcoming', 5)}
+        {dashboard_card('success', 'Payments', 2)}
+        {dashboard_card('danger', 'Failed Payments', 0)}
+      </>
+        : null}
+
+
     </main>
   </>;
 };
